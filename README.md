f4c52592acfb8e8497d27b13689267d80a900701
williamjones62 <williamjones62@gmail.com>
2017-04-22 18:09:30 -0400

    Add code for facebook authorisation, fix email signon, get rid off server code

---
+++ README.md
@@ -0,0 +1,19 @@
+# README
+
+iSweetbriarFE
+
+* AngularJS version 1.5.0
+
+* System dependencies: Auth0 10.14
+
+Project Overview:
+
+This is an Angular JS project providing the front end for viewing online course material and recording student progress. To use this app, simply clone and run http-server from the terminal.
+
+Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/WilliamJones62/i-sweet-briar-fe. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the Contributor Covenant code of conduct.
+
+License
+
+The project is available as open source under the terms of the MIT License.
--- app.js
+++ app.js
@@ -2,13 +2,69 @@
 
   'use strict';
 
-  angular
-    .module('app', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router'])
+  angular.module('authApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router'])
     .config(function($provide, authProvider, $urlRouterProvider, $stateProvider, $httpProvider, jwtInterceptorProvider) {
 
       authProvider.init({
         domain: 'williamjones.auth0.com',
-        clientID: 'LGDLT1jTyvtgBtyDbum7RAJ_QBWyU_nA'
+        clientID: 'vg9XTyhx0_Zo-6gmslOW6BDxfEWPXU1g'
       });
+
+      $urlRouterProvider.otherwise("/home");
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'components/home/home.tpl.html'
+        })
+        .state('profile', {
+          url: '/profile',
+          templateUrl: 'components/profile/profile.tpl.html',
+          controller: 'profileController as user'
+        });
+
+      jwtInterceptorProvider.tokenGetter = function(store) {
+        return store.get('token');
+      }
+
+      function redirect($q, $injector, auth, store, $location) {
+        return {
+          responseError: function(rejection) {
+
+            if (rejection.status === 401) {
+              auth.signout();
+              store.remove('profile');
+              store.remove('token');
+              $location.path('/home')
+            }
+            return $q.reject(rejection);
+          }
+        }
+      }
+      $provide.factory('redirect', redirect);
+      $httpProvider.interceptors.push('jwtInterceptor');
+      $httpProvider.interceptors.push('redirect');
+    })
+    .run(function($rootScope, $state, auth, store, jwtHelper, $location) {
+
+      $rootScope.$on('$locationChangeStart', function() {
+        // Get the JWT that is saved in local storage
+        // and if it is there, check whether it is expired.
+        // If it isn't, set the user's auth state
+        var token = store.get('token');
+        if (token) {
+          if (!jwtHelper.isTokenExpired(token)) {
+            if (!auth.isAuthenticated) {
+              auth.authenticate(store.get('profile'), token);
+            }
+          }
+        }
+        else {
+          // Otherwise, redirect to the home route
+          $location.path('/home');
+        }
+      });
+
     });
+
 })();
