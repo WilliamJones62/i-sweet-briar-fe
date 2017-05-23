4189221dc6b710b476807d8cd07d7e1a31e8ce63
williamjones62 <williamjones62@gmail.com>
2017-05-23 10:47:43 -0400

    The Auth0 examples have been updated

---
+++ auth0-variables.js
@@ -0,0 +1,4 @@
+var AUTH0_CLIENT_ID='{vg9XTyhx0_Zo-6gmslOW6BDxfEWPXU1g}';
+var AUTH0_CLIENT_SECRET='{k9n5nQrx7CUCOqGDe1-tSSGJEfUAWIHz2SUWNGbs11qxL7rDxb9z-TO1dTO6LejS}';
+var AUTH0_DOMAIN='{williamjones.auth0.com}';
+var AUTH0_CALLBACK_URL='http://localhost:3000/callback';
