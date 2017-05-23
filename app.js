654e74b8e3ff4c6b02213f36794c3fed58f389de
williamjones62 <williamjones62@gmail.com>
2017-05-23 10:52:58 -0400

    The Auth0 examples have been updated

--- app.js
+++ app.js
@@ -1,78 +1,56 @@
-(function() {
+(function () {
 
   'use strict';
 
-  angular.module('authApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router'])
-    .config(function($provide, authProvider, $urlRouterProvider, $stateProvider, $httpProvider, jwtInterceptorProvider) {
+  angular
+    .module('app', ['auth0.auth0', 'ui.router'])
+    .config(config);
 
-      authProvider.init({
-        domain: 'williamjones.auth0.com',
-        clientID: 'vg9XTyhx0_Zo-6gmslOW6BDxfEWPXU1g'
-      });
+  config.$inject = [
+    '$stateProvider',
+    '$locationProvider',
+    '$urlRouterProvider',
+    'angularAuth0Provider'
+  ];
 
-      $urlRouterProvider.otherwise("/home");
+  function config(
+    $stateProvider,
+    $locationProvider,
+    $urlRouterProvider,
+    angularAuth0Provider
+  ) {
 
-      $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: 'components/home/home.tpl.html'
-        })
-        .state('profile', {
-          url: '/profile',
-          templateUrl: 'components/profile/profile.tpl.html',
-          controller: 'profileController as user'
-        });
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'HomeController',
+        templateUrl: 'app/home/home.html',
+        controllerAs: 'vm'
+      })
+      .state('callback', {
+        url: '/callback',
+        controller: 'CallbackController',
+        templateUrl: 'app/callback/callback.html',
+        controllerAs: 'vm'
+      });;
 
-      jwtInterceptorProvider.tokenGetter = function(store) {
-        return store.get('token');
-      }
-
-
-
-      function redirect($q, $injector, $timeout, store, $location) {
-
-        var auth;
-        $timeout(function() {
-          auth = $injector.get('auth');
-        });
-
-        return {
-          responseError: function(rejection) {
-
-            if (rejection.status === 401) {
-              auth.signout();
-              store.remove('profile');
-              store.remove('token');
-              $location.path('/home')
-            }
-            return $q.reject(rejection);
-          }
-        }
-      }
-      $provide.factory('redirect', redirect);
-      $httpProvider.interceptors.push('jwtInterceptor');
-      $httpProvider.interceptors.push('redirect');
-    })
-    .run(function($rootScope, $state, auth, store, jwtHelper, $location) {
-
-      $rootScope.$on('$locationChangeStart', function() {
-        // Get the JWT that is saved in local storage
-        // and if it is there, check whether it is expired.
-        // If it isn't, set the user's auth state
-        var token = store.get('token');
-        if (token) {
-          if (!jwtHelper.isTokenExpired(token)) {
-            if (!auth.isAuthenticated) {
-              auth.authenticate(store.get('profile'), token);
-            }
-          }
-        }
-
-        if (!auth.isAuthenticated) {
-          $location.path('/home');
-        }
-      });
-
+    // Initialization for the angular-auth0 library
+    angularAuth0Provider.init({
+      clientID: AUTH0_CLIENT_ID,
+      domain: AUTH0_DOMAIN,
+      responseType: 'token id_token',
+      audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+      redirectUri: AUTH0_CALLBACK_URL,
+      scope: 'openid'
     });
 
-})();
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.hashPrefix('');
+
+    /// Comment out the line below to run the app
+    // without HTML5 mode (will use hashes in routes)
+    $locationProvider.html5Mode(true);
+  }
+
+})();s
